8b46742ffc0c8cad89be2b44a34392ccfcad67e2
tiagobw <tiagotbw@gmail.com>
2023-01-08 02:07:58 -0300

    refactor: add 'to' prop on Button
    adjust button styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
+
 import '../styles/Button.css';
 
-const Button = ({ children }) => {
-  return <button className='button'>{children}</button>;
+const Button = ({ children, to }) => {
+  return to ? (
+    <Link to={to} className='button'>
+      {children}
+    </Link>
+  ) : (
+    <Button className='button'>{children}</Button>
+  );
 };
 
 export default Button;
