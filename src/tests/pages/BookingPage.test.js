3290d7d3b620fe343c8a8ca5af121eef6e087073
tiagobw <tiagotbw@gmail.com>
2023-01-15 13:14:11 -0300

    test: mock initializeTimes and updateTimes

--- src/tests/pages/BookingPage.test.js
+++ src/tests/pages/BookingPage.test.js
@@ -1,9 +1,18 @@
 import { initializeTimes, updateTimes } from '../../pages/BookingPage';
+import * as importedObject from '../../pages/BookingPage';
 
 describe('BookingPage', () => {
   describe('initializeTimes', () => {
     it('returns ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]', () => {
       // arrange
+      importedObject.initializeTimes = jest.fn(() => [
+        '17:00',
+        '18:00',
+        '19:00',
+        '20:00',
+        '21:00',
+        '22:00',
+      ]);
       const sut = initializeTimes;
       const expected = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
 
@@ -18,6 +27,22 @@ describe('BookingPage', () => {
   describe('updateTimes', () => {
     it('returns the same value as its state parameter', () => {
       // arrange
+      const fetchAPI = (date) => [
+        '17:00',
+        '18:00',
+        '19:00',
+        '20:00',
+        '21:00',
+        '22:00',
+      ];
+      importedObject.updateTimes = jest.fn((state, action) => {
+        switch (action.type) {
+          case 'update':
+            return fetchAPI(action.payload);
+          default:
+            throw new Error();
+        }
+      });
       const sut = updateTimes;
       const state = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
       const action = {
